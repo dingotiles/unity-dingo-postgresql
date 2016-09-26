447e438da35612d3c60193ce578846075c038802
Dr Nic Williams <drnicwilliams@gmail.com>
2016-09-26 10:44:54 +0200

    add hololens support

--- Assets/Scripts/CursorMovement.cs
+++ Assets/Scripts/CursorMovement.cs
@@ -19,9 +19,17 @@ public class CursorMovement : Controller<DingoApplication> {
 
 	void ShowCursor()
 	{
-		Ray cameraRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit tileSlotHit;
-		if (Physics.Raycast (cameraRay, out tileSlotHit, camRayLength, tileSlotMask)) {
+        RaycastHit tileSlotHit;
+#if UNITY
+        Ray cameraRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+        var raycast = Physics.Raycast (cameraRay, out tileSlotHit, camRayLength, tileSlotMask);
+#else
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+        var raycast = Physics.Raycast(headPosition, gazeDirection, out tileSlotHit, camRayLength, tileSlotMask);
+#endif
+
+        if (raycast) {
 			TileSlotView tileSlot = tileSlotHit.collider.gameObject.GetComponentInParent<TileSlotView> ();
 			if (tileSlot != null) {
 				tileSlot.EnableCursor (true);
