c3cd50fc85d4c26465e5207c01053c958b18ebb5
Dr Nic Williams <drnicwilliams@gmail.com>
2016-09-26 11:05:46 +0200

    choose camera based on unity vs hololens

---
+++ Assets/Scripts/CameraSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using thelab.mvc;
+
+
+public class CameraSelector : View<DingoApplication>
+{
+    public GameObject unityCamera;
+    public GameObject hololensCamera;
+
+    void Awake()
+    {
+#if UNITY_EDITOR
+        Debug.Log("#UNITY_EDITOR = true");
+        unityCamera.SetActive(true);
+        hololensCamera.SetActive(false);
+#else
+        Debug.Log("#UNITY_EDITOR = false");
+        unityCamera.SetActive(false);
+        hololensCamera.SetActive(true);
+#endif
+    }
+}
